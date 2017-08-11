f0be3f09c90069999802a405760e185ea9e25a06
Lye Guang Xing <gxlye@me.com>
2017-08-11 22:55:28 +0800

    Added Diet and Preferred Cuisine list

---
+++ Foodster/Foodster/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Foodster
+//
+//  Created by Lye Guang Xing on 11/08/2017.
+//  Copyright © 2017 Stencil Code. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public struct Constants {
+    static let diets = ["Gluten-free","Halal","Pork-free","Vegan","Vegetarian"]
+    static let cuisines = ["Chinese","Malay","Indian","Mamak","Western","Korean","Japanese"]
+    
+}
