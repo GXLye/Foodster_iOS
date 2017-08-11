0f0c9f656b8b5ce6b88e7c4ee4f06b84d9d59939
Lye Guang Xing <gxlye@me.com>
2017-08-11 18:39:25 +0800

    Create blank Xcode project

---
+++ Foodster/Foodster/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Foodster
+//
+//  Created by Lye Guang Xing on 11/08/2017.
+//  Copyright © 2017 Stencil Code. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
