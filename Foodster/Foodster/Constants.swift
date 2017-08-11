e61f72de5a4fe122fc3230be2225248f085a85e4
Lye Guang Xing <gxlye@me.com>
2017-08-12 02:52:41 +0800

    Implemented MapView with Restaurant Pins and User location

--- Foodster/Foodster/Constants.swift
+++ Foodster/Foodster/Constants.swift
@@ -21,4 +21,67 @@ public struct Constants {
     static let cuisinesKey = "userCuisines"
     
     
+    static let sampleRest = [Restaurant(name: "Salam Noodles", latitude: "2.926002", longitude: "101.65159", image: ""),
+                             Restaurant(name: "myBurgerLab", latitude: "3.0158647", longitude: "101.5665644", image: ""),
+                             Restaurant(name: "Salam Noodles", latitude: "2.926002", longitude: "101.65159", image: ""),
+                             Restaurant(name: "Salam Noodles", latitude: "2.926002", longitude: "101.65159", image: "")]
+    
+}
+
+class Restaurant : NSObject {
+    var name : String = ""
+    var latitude : String = ""
+    var longitude : String = ""
+    var image : String = ""
+    
+    init(name: String, latitude: String, longitude: String, image: String) {
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+        self.image = image
+    }
+}
+
+extension UIView {
+    func pushTransition(_ duration:CFTimeInterval) {
+        let animation:CATransition = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionPush
+        animation.subtype = kCATransitionFromTop
+        animation.duration = duration
+        self.layer.add(animation, forKey: kCATransitionPush)
+    }
+    
+    func fadeTransition(_ duration:CFTimeInterval) {
+        let animation:CATransition = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionFade
+        animation.duration = duration
+        self.layer.add(animation, forKey: kCATransitionFade)
+    }
+}
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.fadeTransition(0.3)
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+    
 }
--- Foodster/Foodster/FirstViewController.swift
+++ Foodster/Foodster/FirstViewController.swift
@@ -9,16 +9,23 @@
 import UIKit
 import MapKit
 
-class FirstViewController: UIViewController, MKMapViewDelegate {
+class FirstViewController: UIViewController, UITableViewDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+        
         if let diets = UserDefaults.init(suiteName: Constants.groupKey)?.value(forKey: Constants.dietsKey) as? [String] {
             print(diets)
         }
@@ -26,6 +33,20 @@ class FirstViewController: UIViewController, MKMapViewDelegate {
         if let cuisines = UserDefaults.init(suiteName: Constants.groupKey)?.value(forKey: Constants.cuisinesKey) as? [String] {
             print(cuisines)
         }
+        
+        let annotationView = MKPinAnnotationView()
+        annotationView.animatesDrop = true
+        var annotations: [MKPointAnnotation] = []
+        
+        for restaurant in Constants.sampleRest {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(restaurant.latitude)!, longitude: CLLocationDegrees(restaurant.longitude)!)
+            annotation.title = restaurant.name
+//            mapView.addAnnotation(annotation)
+            annotations.append(annotation)
+        }
+        mapView.showAnnotations(annotations, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +54,38 @@ class FirstViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - location manager to authorize user location for Maps app
+    var locationManager = CLLocationManager()
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+}
 
+extension FirstViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Constants.sampleRest[section].name
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        item.textLabel?.text = "Roti Canai"
+        item.detailTextLabel?.text = "Mamak, Vegetarian"
+        item.imageView?.downloadedFrom(link: "http://media.themalaymailonline.com/uploads/articles/Crave/2017/2017-01/LanRoti01.jpg")
+        
+        return item
+    }
 }
 
