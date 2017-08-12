7db4ee8c1807d5c9ac2f0fcdc8cce0faf7236101
Lye Guang Xing <gxlye@me.com>
2017-08-12 13:32:09 +0800

    Added Alamofire

---
+++ Foodster/Pods/Target Support Files/Pods-Foodster/Pods-Foodster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Foodster : NSObject
+@end
+@implementation PodsDummy_Pods_Foodster
+@end
---
+++ Foodster/Pods/Target Support Files/Pods-FoodsterTests/Pods-FoodsterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodsterTests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodsterTests
+@end
